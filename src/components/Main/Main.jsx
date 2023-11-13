4a91672161089206f4196e74ba5a7e6a948c0bd1
Andrey Fedyushev <rdshatv@gmail.com>
2023-11-13 22:32:24 +0700

    test26.2

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Main.css';
 import Header from '../Header/Header';
-import Promo from './Promo/Promo';
-import AboutProject from './AboutProject/AboutProject';
-import Techs from './Techs/Techs';
-import AboutMe from './AboutMe/AboutMe';
-import Portfolio from './Portfolio/Portfolio';
+import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
+import AboutMe from '../AboutMe/AboutMe';
+import Portfolio from '../Portfolio/Portfolio';
 import Footer from '../Footer/Footer';
 
 function Main({ isLoggedIn }) {
