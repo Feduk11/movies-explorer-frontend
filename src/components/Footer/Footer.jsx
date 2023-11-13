75867fa385e7f30d5fd98fb95ee4e69a08e51361
Andrey Fedyushev <rdshatv@gmail.com>
2023-11-13 12:42:03 +0700

    upd footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
-import './Footer.css'
+import './Footer.css';
+
+// Узнаю текущий год
+const currentYear = new Date().getFullYear();
 
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <p className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
       <div className="footer__container">
-        <p className="footer__copyright">&copy;&nbsp;{(new Date().getFullYear())}</p>
-        <ul className="footer__list">
-          <li><a href="https://practicum.yandex.ru" className="footer__link" target="_blank" rel="noreferrer">Яндекс.Практикум</a></li>
-          <li><a href="https://github.com/Feduk11" className="footer__link" target="_blank" rel="noreferrer">Github</a></li>
-        </ul>
+        <p className="footer__data">&copy; {currentYear}</p>
+        <nav className="footer__links">
+          <a
+          href="https://practicum.yandex.ru"
+          className="footer__link"
+          rel="noreferrer"
+          target="_blank"
+          >
+            Яндекс.Практикум
+          </a>
+          <a
+          href="https://github.com/Feduk11"
+          className="footer__link"
+          rel="noreferrer"
+          target="_blank">
+            Github
+          </a>
+        </nav>
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer;
